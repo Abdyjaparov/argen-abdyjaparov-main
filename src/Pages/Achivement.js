e4c4e2b208a891e00f7ad00dee32e8a8d563ef00
Abdyjaparov <159744880+Abdyjaparov@users.noreply.github.com>
2024-11-27 16:09:25 +0600

    i am good

--- src/Pages/Achivement.js
+++ src/Pages/Achivement.js
@@ -3,15 +3,18 @@ import '../Style/Achivement.css';
 
 const Achivement = () => {
     return (
-        <>
-            <div>
+        <> 
+        <div className="achievements">
+            <div className="sport">
                 <h1>My achievements in sports</h1>
                 <h3>Here's the translation with appropriate punctuation:"I don't have any particularly important achievements. I am a multiple champion of my district in my weight class, and I have won several times in international competitions as well as in regional ones.</h3>
             </div>
-            <div>
+            <div className="academic">
                 <h1>My academic achievements</h1>
                 <h3>My academic achievements are not bad. Starting from the 1st grade, I received various certificates in different subjects until the 7th grade. Due to my interests, I stopped studying in the 8th and 9th grades. My achievements in the IT field are also quite good; I have a solid understanding of JavaScript, Python, and React.</h3>
             </div>
+
+        </div>
         </>
         
     )
